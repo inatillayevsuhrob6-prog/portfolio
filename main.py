2e54929c657cf27ba2022b04dfc6254879081c4a
Suxrob <jamshidrahmatullayev80@email.com>
2026-08-18 15:14:05 +0500

    Render deploy uchun tayyorlash

--- main.py
+++ main.py
@@ -22,5 +22,8 @@ def home():
         
     return render_template('index.html')
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
